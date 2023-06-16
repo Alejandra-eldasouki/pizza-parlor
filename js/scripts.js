375de7d7ba0f494ed66402c282f723cc506791a1
Alejae1998 <alejagr1998@gmail.com>
2023-06-16 11:44:48 -0700

    working on js

--- js/scripts.js
+++ js/scripts.js
@@ -6,25 +6,52 @@ function Pizza(toppings, size) {
 
 // Prototype
 Pizza.prototype.calculateTotal = function () {
-  return (
-    5 +
-    this.toppings.length * 0.5 +
-    (this.size === 'personal' ? 0 : this.size === 'medium' ? 2 : 4)
-  );
+  var initialCost = 5;
+  var toppingsCost = this.toppings.length * 1.5;
+  var sizeCost = 0;
+  if (this.size === 'small') {
+    sizeCost = 0;
+  } else if (this.size === 'medium') {
+    sizeCost = 2;
+  } else if (this.size === 'large') {
+    sizeCost = 4;
+  }
+  return initialCost + toppingsCost + sizeCost;
 };
 
 function addPizza() {
   var toppings = [];
-  var checkboxes = document.querySelector(
-    '.toppings input[type="checkbox"]:checked'
-  );
-  checkboxes.forEach(function (checkbox) {
-    toppings.push(checkbox.value);
-  });
-
-  var size = document.querySelector('size input[type="radio"]:checked').value;
+  var toppingsCheckboxes = document.getElementById('toppings');
+  for (var i = 0; i < toppingsCheckboxes.length; i++) {
+    if (toppingsCheckboxes[i]) {
+      toppings.push(toppingsCheckboxes[i].value);
+    }
+  }
+  var size = document.querySelector("select[name='size']"),
+    value;
 
   var pizza = new Pizza(toppings, size);
+  var cost = pizza.calculateTotal();
 
-  var cost = pizza;
+  var pizzaList = document.getElementById('pizzaList');
+  var li = document.createElement('li');
+  li.innerHTML =
+    'Pizza with ' + toppings.join(', ') + ' - ' + size + ' ($' + cost + ')';
+  pizzaList.appendChild(li);
+}
+
+function toggleDeliveryForm() {
+  var deliveryForm = document.getElementById('deliveryForm');
+  if (deliveryForm.style.display === 'none') {
+    deliveryForm.style.display = 'block';
+  } else {
+    deliveryForm.style.display = 'none';
+  }
+}
+
+function submitDelivery() {
+  var addressInput = document.getElementById('addressInput');
+  var address = addressInput.value;
+
+  console.log('Delivery address:', address);
 }
